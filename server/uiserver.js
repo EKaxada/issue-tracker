90616a34e13ae73899eb245d82eaddd5fa791cc8
Enock Kasaadha <enocksdha@gmail.com>
2021-05-13 12:12:59 +0300

    changes for heroku

--- server/uiserver.js
+++ server/uiserver.js
@@ -12,49 +12,49 @@ dotenv.config();
 const enableHMR = (process.env.ENABLE_HMR || 'true') === 'true';
 
 if (enableHMR && (process.env.NODE_ENV !== 'production')) {
-  console.log('Adding dev middlware, enabling HMR');
-  /* eslint "global-require": "off" */
-  /* eslint "import/no-extraneous-dependencies": "off" */
-  const webpack = require('webpack');
-  const devMiddleware = require('webpack-dev-middleware');
-  const hotMiddleware = require('webpack-hot-middleware');
+    console.log('Adding dev middlware, enabling HMR');
+    /* eslint "global-require": "off" */
+    /* eslint "import/no-extraneous-dependencies": "off" */
+    const webpack = require('webpack');
+    const devMiddleware = require('webpack-dev-middleware');
+    const hotMiddleware = require('webpack-hot-middleware');
 
-  const config = require('../webpack.config.js')[0];
-  config.entry.app.push('webpack-hot-middleware/client');
-  config.plugins = config.plugins || [];
-  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+    const config = require('../webpack.config.js')[0];
+    config.entry.app.push('webpack-hot-middleware/client');
+    config.plugins = config.plugins || [];
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
 
-  const compiler = webpack(config);
-  app.use(devMiddleware(compiler));
-  app.use(hotMiddleware(compiler));
+    const compiler = webpack(config);
+    app.use(devMiddleware(compiler));
+    app.use(hotMiddleware(compiler));
 }
 
 app.use(express.static('public'));
 
 const apiProxyTarget = process.env.API_PROXY_TARGET;
 if (apiProxyTarget) {
-  app.use('/graphql', proxy({ target: apiProxyTarget }));
+    app.use('/graphql', proxy({ target: apiProxyTarget }));
 }
 
 if (!process.env.UI_API_ENDPOINT) {
-  process.env.UI_API_ENDPOINT = 'http://localhost:3000/graphql';
+    process.env.UI_API_ENDPOINT = 'https://issue-tracker-app-backend.herokuapp.com/graphql';
 }
 
 app.get('/env.js', (req, res) => {
-  const env = { UI_API_ENDPOINT: process.env.UI_API_ENDPOINT };
-  res.send(`window.ENV = ${JSON.stringify(env)}`);
+    const env = { UI_API_ENDPOINT: process.env.UI_API_ENDPOINT };
+    res.send(`window.ENV = ${JSON.stringify(env)}`);
 });
 
 app.get('*', (req, res, next) => {
-  render(req, res, next);
+    render(req, res, next);
 });
 
-const port = process.env.UI_SERVER_PORT || 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`UI started on port ${port}`);
+    console.log(`UI started on port ${port}`);
 });
 
 if (module.hot) {
-  module.hot.accept('./render.jsx');
+    module.hot.accept('./render.jsx');
 }
